2386cc46cddfaee2a20369f5255ef314eaa14f21
Patrick Clarke <patrick@newmediacampaigns.com>
2018-09-07 10:47:52 -0400

    Update ESLint settings, player data

--- components/Player.js
+++ components/Player.js
@@ -1,15 +1,19 @@
 // import react library
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Player extends Component {
   render() {
     const {
-      first, last, rank, gp, from, to,
+      first,
+      last,
+      rank: { year2017 },
+      gp,
+      from,
+      to
     } = this.props.details;
-
     return (
       <tr>
-        <th>{rank}</th>
+        <th>{rank.year2017}</th>
         <td>{`${first} ${last}`}</td>
         <td>{gp}</td>
         <td>{`${from} ${to}`}</td>
